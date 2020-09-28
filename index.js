890f9518e61ef085c907c8e9ceff34ddf676d6f2
kanishk <kanishk.9871@gmail.com>
2020-09-28 13:37:28 +0530

    https://github.com/kanishk014/clakk.git

---
+++ index.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const http = require("http");
+const { dirname } = require("path");
+const url = require("url");
+
+
+const PropertySingle = fs.readFileSync(
+  `${__dirname}/property-single.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+//   if (pathname === "/" || pathname === "/home") {
+//     res.writeHead(200, {
+//       "Content-type": "text/html",
+//     });
+
+//     const cardsHtml = dataObj
+//       .map((el) => replaceTemplate(tempCard, el))
+//       .join("");
+//     const output = PropertySingle.replace("{%PRODUCT_CARDS%}", cardsHtml);
+//     res.end(output);
+
+    // Product page
+  } else if (pathname === "/property") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(PropertySingle, product);
+    res.end(output);
+
+    // API
+  } else if (pathname === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+
+    // Not found
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
